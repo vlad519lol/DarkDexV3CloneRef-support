d2f340c8ddf63f0b79e2b3ea56dc5cefc3a1c578
vlad519lol <55244263+vlad519lol@users.noreply.github.com>
2025-01-14 04:11:47 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,4 @@
+--[[
+	This script has been verified by desurass and it works just fine
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vlad519lol/DarkDexV3CloneRef-support/refs/heads/main/DarkDexV3.lua", true))()
